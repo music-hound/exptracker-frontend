fff932b75f2dfb284df952f2e652c708baec7f24
Igor <musichound.ge@gmail.com>
2025-11-07 12:37:17 +0500

    env fix

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromLocalStorage } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3030/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Authorization: 'Bearer ' + getTokenFromLocalStorage(),
   }
